f4d9a8f0a5b7e710ab85e5ccdbc693ac2f41790e
daryl john silvestre <daryljann28@gmail.com>
2023-01-21 15:37:47 +0800

    daryljohn midterm

---
+++ Infrastructure/Domain/Model/Courses.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace daryljohn.midterm_.Infrastructure.Domain.Model
+{
+    public class Courses : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Infrastructure/Domain/Model/Student.cs
@@ -0,0 +1,27 @@
+﻿using daryljohn.midterm_.Infrastructure.Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+using daryljohn.midterm_.Infrastructure.Domain.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.PortableExecutable;
+
+namespace daryljohn.midterm_.Infrastructure.Domain.Model
+{
+    public class Products
+    {
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+        public string? EmailAddress { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public Type? Type { get; set; }
+        public Guid? RoleId { get; set; }
+
+        [ForeignKey("RoleId")]
+        public Categories? Categories { get; set; }
+    }
+
+    public enum Type
+    {
+        Consumable = 1,
+        NonConsumable = 2
+    }
+}
