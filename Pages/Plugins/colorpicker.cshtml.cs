9d8946bae7c3f7d3649845fa626f1c058e037050
Daryl john silvestre <daryljann28@gmail.com>
2023-01-28 13:28:07 +0800

    finalsexamandactivuty

---
+++ Pages/Plugins/colorpicker.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace daryljohn.midterm_.Pages.Plugins
+{
+    public class colorpickerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
